81b0c2a98d9f9f56346938c9e70be8e859420386
jj1980a <j.feral@live.fr>
2024-02-12 11:45:49 +0800

    update contracts addresses on sepolia

--- src/address.ts
+++ src/address.ts
@@ -1,14 +1,14 @@
 export const atlasAddress: { [chainId: number]: string } = {
   1: "0x1", // Ethereum Mainnet
-  11155111: "0x11155111", // Ethereum Sepolia
+  11155111: "0x644256165e9aC5F617370AB7cecCBD989aC96181", // Ethereum Sepolia
 };
 
 export const atlasVerificationAddress: { [chainId: number]: string } = {
   1: "0x1", // Ethereum Mainnet
-  11155111: "0x11155111", // Ethereum Sepolia
+  11155111: "0x92e3509a76BB7219e5F546943B6d2403f52Aa9D3", // Ethereum Sepolia
 };
 
 export const atlasSorterAddress: { [chainId: number]: string } = {
   1: "0x1", // Ethereum Mainnet
-  11155111: "0x11155111", // Ethereum Sepolia
+  11155111: "0x7a2f42623477B6875590ff7982B5485431ED762E", // Ethereum Sepolia
 };
