6be9a9f41ffc8d74be3470dfe2c46a68fab891f0
jj1980a <j.feral@live.fr>
2024-05-07 17:16:06 +0400

    cleanup dependencies and testing environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,4 @@
 export default {
   preset: "ts-jest/presets/js-with-ts-esm",
   moduleDirectories: ["node_modules", "<rootDir>"],
-  testEnvironment: "./jsdomEnvironment.ts",
-  setupFilesAfterEnv: ["./jest.setup.ts"],
 };
--- jest.setup.ts
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(Uint8Array, Symbol.hasInstance, {
-  value(potentialInstance: unknown) {
-    return this === Uint8Array
-      ? Object.prototype.toString.call(potentialInstance) ===
-          "[object Uint8Array]"
-      : Uint8Array[Symbol.hasInstance].call(this, potentialInstance);
-  },
-});
