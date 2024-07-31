f364b1e5b0ea7c8cb8a0db24de42290afe435e40
jj1980a <j.feral@live.fr>
2024-07-31 11:00:41 +0400

    add polygon mainnet and amoy support

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -49,7 +49,7 @@ export const chainConfig: { [chainId: number]: ChainConfig } = {
     },
   },
 
-  // Sepolia
+  // Ethereum Sepolia
   11155111: {
     contracts: {
       atlas: {
@@ -73,6 +73,60 @@ export const chainConfig: { [chainId: number]: ChainConfig } = {
       version: "1.0",
       chainId: 11155111,
       verifyingContract: "0xB6F66a1b7cec02324D83c8DEA192818cA23A08B3",
+    },
+  },
+
+  // Polygon Mainnet
+  137: {
+    contracts: {
+      atlas: {
+        address: "0x892F8f6779ca6927c1A6Cc74319e03d2abEf18D5",
+      },
+      atlasVerification: {
+        address: "0xc05DDBe9745ce9DB45C32F5e4C1DA7a3c4FDa220",
+      },
+      sorter: {
+        address: "0x81f1E70A11A9E10Fa314cC093D149E5ec56EE97f",
+      },
+      simulator: {
+        address: "0xfBc81A39459E0D82EC31B4e585f7A318AFAdB49B",
+      },
+      multicall3: {
+        address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      },
+    },
+    eip712Domain: {
+      name: "AtlasVerification",
+      version: "1.0",
+      chainId: 137,
+      verifyingContract: "0xc05DDBe9745ce9DB45C32F5e4C1DA7a3c4FDa220",
+    },
+  },
+
+  // Polygon Amoy
+  80002: {
+    contracts: {
+      atlas: {
+        address: "0x282BdDFF5e58793AcAb65438b257Dbd15A8745C9",
+      },
+      atlasVerification: {
+        address: "0x3b7B38362bB7E2F000Cd2432343F3483F785F435",
+      },
+      sorter: {
+        address: "0xa55051bd82eFeA1dD487875C84fE9c016859659B",
+      },
+      simulator: {
+        address: "0x3efbaBE0ee916A4677D281c417E895a3e7411Ac2",
+      },
+      multicall3: {
+        address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      },
+    },
+    eip712Domain: {
+      name: "AtlasVerification",
+      version: "1.0",
+      chainId: 137,
+      verifyingContract: "0x3b7B38362bB7E2F000Cd2432343F3483F785F435",
     },
   },
 };
